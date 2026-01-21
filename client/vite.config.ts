f3c1dfa6b67a1d5872e1a2e85ff37fe1541c0c51
Redjan Rajabaly <redjanrajabaly@gmail.com>
2026-01-21 14:58:02 +0100

    essaie CI/CD

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,16 @@
-import basicSsl from "@vitejs/plugin-basic-ssl";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
     host: true,
+    port: 4173,
     proxy: {
-      "/api": {
-        target: "http://localhost:3000",
+      '/api': {
+        target: 'http://localhost:3000',
         changeOrigin: true,
-        secure: false,
       },
     },
   },
-});
+})
